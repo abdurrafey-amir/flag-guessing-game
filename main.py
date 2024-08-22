1f9574183de19d9497644c95807d88f3d1d86036
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-08-23 00:39:37 +0500

    got random flag, made basic cli prototype thingie

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import random
+
+countries = requests.get('https://flagcdn.com/en/codes.json').json()
+country_names = list(countries.values())
+country_codes = list(countries.keys())
+country_name = random.choice(country_names)
+country_code = random.choice(country_codes)
+print(country_code)
+
+# flag
+flag  = requests.get(f'https://flagcdn.com/224x168/{country_code}.png')
+# print(flag.status_code)
+with open(f'{country_code}.png', 'wb') as f:
+    f.write(flag.content)
+
+# guess the country
+print("Guess the country")
+guess = input("Enter the country: ")
+if guess == country_name:
+    print("Correct!")
+else:
+    print(f"Incorrect! The country was {country_name}")
