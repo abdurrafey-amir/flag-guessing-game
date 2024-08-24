540d43e009fba7ab309c5f31411dcc2ddbe4019b
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-08-24 16:48:06 +0500

    worked on clicks and reset

--- game.py
+++ game.py
@@ -30,7 +30,7 @@ clock = pygame.time.Clock()
 
 # other rects
 flagbox_rect = pygame.FRect(50, 50, 700, 300)
-flag_surf = pygame.image.load('flag.png').convert()
+flag_surf = pygame.image.load('flag.png').convert_alpha()
 flag_surf = pygame.transform.scale(flag_surf, (600, 260))
 flag_rect = flag_surf.get_frect(center=(WIDTH/2, 200))
 
@@ -44,11 +44,13 @@ grey = pygame.Color('grey12')
 
 # print(country_names)
 options = country_names
+rects = []
 
 def show_options():
     for col in range(2):
         for row in range(2):
             rect = pygame.Rect(col * 350 + 100, 380 + row * 120, 250, 80)
+            rects.append(rect)
             text = font.render(options[col * 2 + row], True, (255, 255, 255))
             screen.blit(text, (rect.x + 20, rect.y + 20))
             pygame.draw.rect(screen, (255, 255, 255), rect, 2)
@@ -61,17 +63,44 @@ running = True
 while running:
 
     clock.tick(60)
-
-    # events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        
+    
+    
     
     screen.fill(grey)
     show_options()
     pygame.draw.rect(screen, (255, 255, 255), flagbox_rect, 2)
     screen.blit(flag_surf, flag_rect)
+    
+    
+    # events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEMOTION:
+            for rect in rects:
+                if rect.collidepoint(event.pos):
+                    pygame.draw.rect(screen, (255, 0, 0), rect, 2)
+                    print('test')
+                else:
+                    pygame.draw.rect(screen, (255, 255, 255), rect, 2)
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for i, rect in enumerate(rects):
+                if rect.collidepoint(event.pos):
+                    if options[i] == country_names[i]:
+                        print("Correct!")
+                    else:
+                        print("Wrong!")
+                        
+                    get_flag()
+                    show_options()
+                    flag_surf = pygame.image.load('flag.png').convert_alpha()
+                    flag_surf = pygame.transform.scale(flag_surf, (600, 260))
+                    flag_rect = flag_surf.get_frect(center=(WIDTH/2, 200))
+                    options = country_names
+                    rects = []
+                    
+                    break
 
 
     pygame.display.flip()
